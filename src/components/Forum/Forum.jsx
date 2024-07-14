beb751c421d61d3562bce8cb61d4b553d3135d91
xAEvi <105865102+xAEvi@users.noreply.github.com>
2024-07-14 15:10:34 -0500

    Added Forum page

---
+++ src/components/Forum/Forum.jsx
@@ -0,0 +1,13 @@
+import Header from "../Header/Header";
+import "./Forum.css";
+
+const Forum = () => {
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
+};
+
+export default Forum;
